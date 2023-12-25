12fef7c89cc7ac1ec6d608f8fd536f677fb0e635
psinha87 <109030841+psinha87@users.noreply.github.com>
2023-12-25 16:43:34 +0530

    Update main.tf

--- infra/rds-db-instance/main.tf
+++ infra/rds-db-instance/main.tf
@@ -10,7 +10,7 @@ resource "aws_db_instance" "default" {
     storage_type = "gp2"
     engine = "mysql"
     engine_version = "5.7"
-    instance_class = "db.t3.micro"
+    instance_class = "db.t2.micro"
     identifier = "mydb"
     username = "dbuser"
     password = "dbpassword"
