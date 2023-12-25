9a1c22ee733759d84af2b092f0cd975eedcb275b
psinha87 <109030841+psinha87@users.noreply.github.com>
2023-12-25 16:42:10 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -22,7 +22,7 @@ module "python" {
     subnet-id = tolist(module.networking.public-subnet-id)[0]
     enable-public-ip-address = var.enable-public-ip-address
    // user-data-install-jenkins = templatefile("./jenkins/install-jenkins-with-terraform.sh", {})
-    public-key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCOc3jYzQaIuFNs9pl7N/kcfmWPgyhitrUHVbwaZlkajftbXNgEtxkoBKhb9bLpXCUKc29cIoX3skgVkTijMVzU6rVioL/f/tpd5X+LBCzlB6YyAfarmVwukK2+3rGqNt2/3ezJ7Tgy5p/HeTTTJ/wymt67Zrn6W03wh+yfFxYXoLjCPY7nCFoKjaIhgx9P+QJkene7toOZmkQ+ogTt014sRlBppNFSzZcwvYUqNjpMtu1VQ+f+iIUpWVefUoWVAt3cvrzSpTxwrv9kTJjOhUeaaAc9vNBGMVI5PX2T1oz/Qhvnr+71fD0KmAN4wp7SQUw1+4cjje5BM118c1v1gJBnfcA1kf1UWqzlXaX23FkUqyjvvmZtsGbTd+gFHGuYla2ky/Tw6S47I6fcWshwoGoBRILx39igUB1vAaYN7Q3iRMw1xS8cHmx1g2wTqYO6ULJ6IrA4pj9mTemg+/CHygmBy1wEa76k4zVwTqVAQGntY/3OelEVWE4ePZ/Z2aOTOuc= ec2-user@ip-172-31-28-160.eu-north-1.compute.internal"
+    public-key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCoMZ4VzjPSy8isCQFHZTOIGz7o9BjdTsISthbKBRFBvxYdXnTkbphTLOnd5l9CjtWaCFGEReatIFRbtgOH9ArQkr27Wln9hbl24n7Z8W9t4efhrBLBxvvOb9fdwyJe143UQDlbM0NOVE2/ogd00un0iUAYc5OEcZBjPYef1GXBVN+uYzx5PVkEboZj8hZsp2FrSA385fcH+IBxLVzk3NlUbdgpf2iEJyOXagu4X65bhWE9v0ph6YpRCXZvfWSvE0x4wtKumhvuZDARsx7B3Ig6AVKHowU6V4Q35rmY4KmO/YmNs+1xjFGt1kIh4wRjs74FbOo8cQYlW/uVnrXQbfA9WvyTvOXQWArZBv86dDjzBHcmA/h5/copvxmyALXMZ+nugEEaBStPOAOy4AU/ippuQ0UdQeEPEaSQcW4Q7QqR2KPb4BfbqNuyUXFrjCxaAQm0yS5U46aSsIi6FXAxwqgZxeCfP2geMl8q8RkcD9AZKTs7zrZA0cbfSXaiiZ5qxeE= ec2-user@ip-172-31-28-160.eu-north-1.compute.internal"
 }
 module "lb-target-group"{
     source = ".//lb_target_group"
